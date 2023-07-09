3eea5068037965d0956f6001c0ebff069757dce6
TexTrue <3140846162@qq.com>
2023-07-10 07:54:00 +0800

    add cloth-config dependencies in modinfo, fix config

--- fabric/src/main/java/team/teampotato/memorycleanermissnotoredict/fabric/MCMODModClientFabric.java
+++ fabric/src/main/java/team/teampotato/memorycleanermissnotoredict/fabric/MCMODModClientFabric.java
@@ -3,12 +3,15 @@ package team.teampotato.memorycleanermissnotoredict.fabric;
 import net.fabricmc.api.ClientModInitializer;
 
 import team.teampotato.memorycleanermissnotoredict.MCMODModClient;
+import team.teampotato.memorycleanermissnotoredict.config.ConfigUtil;
 import team.teampotato.memorycleanermissnotoredict.fabric.command.ClientCommand;
 
 public class MCMODModClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MCMODModClient.initClient();
-        ClientCommand.registerClientCommand();
+        if (ConfigUtil.getConfig().enableCommand) {
+            ClientCommand.registerClientCommand();
+        }
     }
 }
