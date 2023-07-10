34965eaf9da614298cdd5d6bbfffdfc795b75dc4
TexTrue <3140846162@qq.com>
2023-07-10 14:47:04 +0800

    config comment & tooltip

--- common/src/main/java/team/teampotato/memorycleanermissnotoredict/config/ModConfigScreen.java
+++ common/src/main/java/team/teampotato/memorycleanermissnotoredict/config/ModConfigScreen.java
@@ -2,11 +2,21 @@ package team.teampotato.memorycleanermissnotoredict.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import team.teampotato.memorycleanermissnotoredict.MCMODMod;
 
 @Config(name = MCMODMod.MOD_ID)
 public class ModConfigScreen implements ConfigData {
+    @Comment("Whether to enable countdown auto-clean")
+    @ConfigEntry.Gui.Tooltip
     public boolean autoCleanMemory = true;
+
+    @Comment("Whether to enable the client manual cleanup command")
+    @ConfigEntry.Gui.Tooltip
     public boolean enableCommand = true;
+
+    @Comment("Clear interval time in minutes (default is 8)")
+    @ConfigEntry.Gui.Tooltip
     public long cleanupInterval = 8;
 }
